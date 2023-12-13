ea0c79fdeff9d98c6f56f3a1f25d2b50c79b8c0a
Cyril FERRER <cyril.ferrer31@gmail.com>
2023-12-13 11:34:25 +0100

    implémentation ajout de projet

--- editpage.js
+++ editpage.js
@@ -22,3 +22,29 @@ export function revealEditMode(key){
     })
 }
 
+export function generateSelectOptions(listeWorks){
+    //création d'un set pour lister les catégories sans doublon
+    const categoriesSet = new Set()
+    listeWorks.forEach((element) =>{
+        categoriesSet.add(element.category.name)
+    })
+    //récupération du "select" récipiendaire
+    const formulaire = document.getElementById("category")
+    //ajout des catégories au select
+    categoriesSet.forEach((element) =>{
+        const option = document.createElement("option")
+        switch(element){
+            case "Appartements":
+                option.value = 2
+                break
+            case "Hotels & restaurants":
+                option.value = 3
+                break
+            default:
+                option.value = 1
+        }
+        option.innerText = element
+        formulaire.appendChild(option)
+    })
+    console.log(categoriesSet)
+}
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import {generateGallery, eraseGallery, generateFilterTags, generateModalGallery} from "./galerie.js"
-import { revealEditMode } from "./editpage.js"
+import { revealEditMode, generateSelectOptions } from "./editpage.js"
 
 
 const storedToken = window.localStorage.getItem("key")
@@ -46,4 +46,37 @@ photoFormCloser.addEventListener("click", ()=>{
 })
 photFormReturn.addEventListener("click", ()=>{
     wrapperPhotoForm.style.display = "none"
+})
+
+const inputAddPicture = document.getElementById("choosedPicture")
+const picturingChoice = document.querySelector(".picturingChoice")
+let picture =""
+inputAddPicture.addEventListener("change", (event)=>{
+    console.log(event.target.value)
+    picturingChoice.src = URL.createObjectURL(event.target.files[0])
+    picture = event.target.files[0]
+})
+
+generateSelectOptions(works)
+
+const buttonValidate = document.querySelector(".modal__button--validate")
+buttonValidate.addEventListener("click", ()=>{
+    const formData = new FormData()
+    const image = document.querySelector(".picturingChoice")
+    const title = document.getElementById("title")
+    const category = document.getElementById("category")
+    formData.append("image", picture)
+    formData.append("title", title.value)
+    formData.append("category", category.value)
+    console.log(formData)
+    console.log(title.value)
+    console.log(category.value)
+    console.log(picturingChoice.src)
+    console.log(picture)
+    const checkedToken = window.localStorage.getItem("key")
+    fetch("http://localhost:5678/api/works", {
+        method: "POST",
+        headers: {Authorization: `Bearer ${checkedToken}`},
+        body: formData,
+    })
 })
